f4372b0fa883bd8acd2dbf0b119b27d860024794
Dot Om <dotunomosebi@gmail.com>
2025-10-18 23:24:27 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,51 @@
+# print("Variable")
+# print("Variable Naming Rules")
+# print("Data Types")
+# print("Type Conversion")
+
+
+a = 56 #
+
+print(a)
+
+name = "James"
+print(name)
+
+temp = 34.6
+print(temp)
+
+on_the_right_way = True
+print(on_the_right_way)
+
+is_awake = False
+print(is_awake)
+
+carEngine = 1  # OOP - Java, C#.NET
+
+car_engine = 1
+
+# int
+age = 25
+print(age)
+
+
+# string
+his_name = "Kirk"
+print(his_name)
+
+
+# bool
+awake = True
+print(awake)
+
+
+# float
+temp = 35.9883434
+print(temp)
+
+print("##############")
+temp = "35.5"
+print(temp + " C")
+
+print(int(float(temp)))
+print(float(temp))
